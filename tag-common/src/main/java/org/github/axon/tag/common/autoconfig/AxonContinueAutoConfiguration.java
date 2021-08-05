b425538bf47fbdadb0074e639a1bdc46e696989f
leechedan <leechedan@gmail.com>
2021-08-05 15:42:24 +0800

    fixed

--- tag-common/src/main/java/org/github/axon/tag/common/autoconfig/AxonContinueAutoConfiguration.java
+++ tag-common/src/main/java/org/github/axon/tag/common/autoconfig/AxonContinueAutoConfiguration.java
@@ -1,6 +1,8 @@
 package org.github.axon.tag.common.autoconfig;
 
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.common.jdbc.PersistenceExceptionResolver;
+import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.ConfigurationScopeAwareProvider;
 import org.axonframework.config.EventProcessingConfigurer;
@@ -11,10 +13,15 @@ import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.upcasting.event.EventUpcaster;
 import org.axonframework.spring.config.AxonConfiguration;
 import org.axonframework.spring.eventsourcing.SpringAggregateSnapshotterFactoryBean;
 import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
+import org.github.axon.tag.common.continuance.common.CustomJpaEventStorageEngine;
 import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -27,7 +34,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @AutoConfigureBefore(AxonAutoConfiguration.class)
 @EnableJpaRepositories("org.github.axon.tag.common.repository")
 @EntityScan(basePackages = {
-//        "org.axonframework.eventsourcing.eventstore.jpa",
         "org.axonframework.eventhandling.tokenstore.jpa",
         "org.axonframework.modelling.saga.repository.jpa",
         "org.github.axon.tag.common.continuance.common"
@@ -67,4 +73,23 @@ public class AxonContinueAutoConfiguration {
     public ListenerInvocationErrorHandler listenerInvocationErrorHandler() {
         return PropagatingErrorHandler.INSTANCE;
     }
+
+    @Bean
+    @Primary
+    public EventStorageEngine eventStorageEngine(Serializer defaultSerializer,
+                                                 PersistenceExceptionResolver persistenceExceptionResolver,
+                                                 @Qualifier("eventSerializer") Serializer eventSerializer,
+                                                 EntityManagerProvider entityManagerProvider,
+                                                 EventUpcaster userUpCaster,
+                                                 TransactionManager transactionManager) {
+        return CustomJpaEventStorageEngine.builder()
+                                          .snapshotSerializer(defaultSerializer)
+                                          .upcasterChain(userUpCaster)
+                                          .persistenceExceptionResolver(persistenceExceptionResolver)
+                                          .eventSerializer(eventSerializer)
+                                          .entityManagerProvider(entityManagerProvider)
+                                          .transactionManager(transactionManager)
+                                          .build();
+    }
+
 }
